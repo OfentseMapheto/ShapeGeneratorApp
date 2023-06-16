b33f5f88026fddc762911a09267341c242a20fb8
Ofentse Mapheto <134792956+OfentseMapheto@users.noreply.github.com>
2023-06-16 15:15:41 +0200

    Add files via upload

---
+++ Ofentse_Assignment1_WPR371.js
@@ -0,0 +1,60 @@
+const readline = require('readline');
+
+// Function to draw a triangle
+function drawTriangle(size, character) {
+  for (let i = 1; i <= size; i++) {
+    console.log(character.repeat(i));
+  }
+}
+
+// Function to draw a square 
+function drawSquare(size, character) {
+  for (let i = 1; i <= size; i++) {
+    console.log(character.repeat(size));
+  }
+}
+
+// Function to draw a diamond
+function drawDiamond(size, character) {
+  const middleIndex = Math.ceil(size / 2);
+
+  for (let i = 1; i <= size; i++) {
+    const spaces = Math.abs(middleIndex - i);
+    const stars = size - 2 * spaces;
+    console.log(' '.repeat(spaces) + character.repeat(stars));
+  }
+}
+
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+
+rl.question('Select a shape: \n1. Triangle\n2. Square\n3. Diamond\n', shape => {
+
+  rl.question('Enter the size of the shape: ', size => {
+    size = parseInt(size);
+
+    
+    rl.question('Enter the character to draw the shape: ', character => {
+      switch (shape) {
+        case '1':
+          drawTriangle(size, character);
+          break;
+        case '2':
+          drawSquare(size, character);
+          break;
+        case '3':
+          drawDiamond(size, character);
+          break;
+        default:
+          console.log('Invalid shape selection.');
+      }
+
+    
+      rl.close();
+    });
+  });
+});
